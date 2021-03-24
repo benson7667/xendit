cfd8993daf7b14bcf0905c7d39e93a3f92c74b7e
bensontoh <benson_7667@hotmail.com>
2021-03-24 22:30:28 +0800

    chore: added extra paragraph

--- src/pages/newsletter/newsletter.jsx
+++ src/pages/newsletter/newsletter.jsx
@@ -14,14 +14,19 @@ const NewsLetter = () => {
   return (
     <Container>
       <Navbar />
-      <div className='newsletter__container'>
-        <h2>Subscribe newsletter</h2>
-
-        <div className='newsletter__info'>
-          <Input onChange={handleOnChange} placeholder='Email address' style={{ marginRight: '5px' }} value={email} />
-          <Button onClick={handleOnClick} value='Subscribe' />
+      <form onSubmit={handleOnClick}>
+        <div className='newsletter__container'>
+          <h2>Subscribe newsletter</h2>
+          <p>
+            Hmm... Requirement for 3(c) is unclear, would you like me to setup a json server and store the user into
+            users.json when they subscribe to the newsletter? 🤔 🤔 🤔
+          </p>
+          <div className='newsletter__info'>
+            <Input onChange={handleOnChange} placeholder='Email address' style={{ marginRight: '5px' }} value={email} />
+            <Button onClick={handleOnClick} value='Subscribe' />
+          </div>
         </div>
-      </div>
+      </form>
     </Container>
   )
 }
